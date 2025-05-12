b8a144c871c097ed2b0e7d0007cb3c3145de7485
maurov1ctor <maurocarvalho745@gmail.com>
2025-05-12 15:07:25 -0300

    att v1.0

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,47 @@
 import java.util.Scanner;
 
 public class Calculator {
-
-    Scanner in = new Scanner(System.in);
-
     float num1;
     float num2;
     float res;
+    String ope;
 
     public void Calc(){
-        System.out.println("\n************ PROGRAMA CALCULATOR ************ \n");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\n************ PROGRAMA CALCULATOR ************\n");
         System.out.print("Entre com um número: \n");
-        num1 = in.nextInt();
+        num1 = in.nextFloat();
 
         System.out.print("Entre com outro número: \n");
-        num2 = in.nextInt();;
+        num2 = in.nextFloat();
+
+        System.out.println("Escolha qual operaçãop será realizada: ( + - / * ");
+        ope = in.next();
+
+        switch (ope){
+            case "+":
+                res = num1 + num2;
+                    break;
+
+            case "-":
+                res = num1 - num2;
+                    break;
+
+            case "*":
+                res = num1 * num2;
+                break;
+
+            case "/":
+                res = num1 / num2;
+                break;
+
+            default:
+                System.out.println("Operação inválida.");
+                return;
+        }
+
+        System.out.println(String.format("Resultado: %.2f", res));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
+        calculator.Calc();
     }
 }
